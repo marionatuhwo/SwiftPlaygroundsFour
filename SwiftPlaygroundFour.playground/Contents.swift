5419e70e8a049235622a8bd21c2ff4786075b5b8
Marion <ano@hawaii.edu>
2021-02-23 10:05:56 -1000

    Completed Parts 1-6

---
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Part 4
+/*for number in 1...5
+{
+
+    print(number)
+}*/
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print(sum)
+}
+
+//Part 6 While Loop
+var i = 1
+while i <= 10
+{
+    
+    i = i + 1
+    print(i)
+}
+
+
+
