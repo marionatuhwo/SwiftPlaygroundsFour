e0100ecfc8fa09d3e0838e94aba180dc725a50cb
Marion <ano@hawaii.edu>
2021-03-08 09:23:18 -1000

    Completed Problem Set with Comments

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -3,25 +3,33 @@ import UIKit
 var str = "Hello, playground"
 
 //Part 4
-/*for number in 1...5
-{
-
-    print(number)
-}*/
-
-var sum = 0
+// for-in loop for range 1-5
 for number in 1...5
 {
+    //print each number in range
+    print(number)
+}
+//declaration of variable "sum"
+var sum = 0
+// for-in loop for range 1-5
+
+for number in 1...5
+{
+    //increment sum by number
     sum += number
+    //print sum
     print(sum)
 }
 
 //Part 6 While Loop
+//declaration of var "i"
 var i = 1
+//while "i" is less than or equal to 10
 while i <= 10
 {
-    
+    //add i + 1
     i = i + 1
+    //print "i"
     print(i)
 }
 
